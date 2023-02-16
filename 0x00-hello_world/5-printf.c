530432bd08d8f00145f20d8faa0ba6b8a90b0156
quasimotojnr <eseniemogha@yahoo.com>
2023-02-16 14:34:25 -0800

    Hot fix

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,7 +6,7 @@
  *
  * Return: Always 0 (Success)
  */
-Int main(void)
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,11 +15,11 @@ int main(void)
 	long long int longLongIntSize;
 	float floatSize;
 
-	printf("Size of a char : %zu byte(s)\n" sizeof(charSize))
-	printf("Size of an int : %zu byte(s)\n" sizeof(intSize))
-	printf("Size of a long int : %zu byte(s)\n" sizeof(longIntSize))
-	printf("Size of a long long int : %zu byte(s)\n" sizeof(longLongIntSize))
-	printf("Size of a float : %zu byte(s)\n" sizeof(floatSize))
+	printf("Size of a char : %ld byte(s)\n" sizeof(charSize))
+	printf("Size of an int : %ld byte(s)\n" sizeof(intSize))
+	printf("Size of a long int : %ld byte(s)\n" sizeof(longIntSize))
+	printf("Size of a long long int : %ld byte(s)\n" sizeof(longLongIntSize))
+	printf("Size of a float : %ld byte(s)\n" sizeof(floatSize))
 
 	return (0)
 }
