6cbe31609ef1e0acbffbb68660761ffb11e682e7
quasimotojnr <eseniemogha@yahoo.com>
2023-02-21 15:41:13 -0800

    Task 1 and 2 done

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+/**
+ * main - printing a string
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("_putchar");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+/**
+ * print_alphabet(void) - function for printing alphabets.
+ *
+ * Return: Always void.
+ */
+void print_alphabet(void)
+{
+	char alpha = 'a';
+	int ascii = 97;
+
+	while (ascii < 123)
+	{
+		putchar(alpha);
+		alpha++;
+		ascii++;
+	}
+	putchar('\n');
+}
