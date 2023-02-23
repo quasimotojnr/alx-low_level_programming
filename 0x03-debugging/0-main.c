48de72987005f1bf4c47bb88e53954fb4f686741
quasimotojnr <eseniemogha@yahoo.com>
2023-02-23 02:08:05 -0800

    Task 1 and 2 done

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Programme to determine if number is positive or negetive
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
