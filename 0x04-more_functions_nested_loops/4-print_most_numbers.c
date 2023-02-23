f29b20f20d81a6813c21e72a2582cfa23dbec4a3
quasimotojnr <eseniemogha@yahoo.com>
2023-02-23 07:14:32 -0800

    Tasks 5 done

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * print_most_numbers - Functions for printing numbers
+ * 0 to 9 except 2 and 4
+ *
+ * Return: Always 0 (Success)
+ */
+void print_most_numbers(void)
+{
+	int num = 48;
+	int ascii = 0;
+
+	while (ascii < 10)
+	{
+		if (ascii == 2 || ascii == 4)
+		{
+			continue;
+		}
+		putchar(num);
+		num++;
+		ascii++;
+	}
+	putchar('\n');
+}
+
