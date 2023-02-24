e383b62ef15b11438d89824a8216f13fadf8f8f3
quasimotojnr <eseniemogha@yahoo.com>
2023-02-24 06:21:50 -0800

    Voluntary task 2 done

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+/**
+ * print_number - Prints any integer number higher than -10000 and
+ * less than 10000
+ * @n: Integer argument to be printed
+ *
+ * Return: void
+ */
+void print_number(int n)
+{
+	int a, b, c, d, num;
+
+	if (n < 0)
+	{
+		putchar('-');
+		num = n * -1;
+	}
+	else
+	{
+		num = n;
+	}
+	if (num >= 1000 && num < 10000)
+	{
+		a = num / 1000;
+		b = (num / 100) % 10;
+		c = (num / 10) % 10;
+		d = num % 10;
+		putchar(a + '0');
+		putchar(b + '0');
+		putchar(c + '0');
+		putchar(d + '0');
+	}
+	else if (num >= 100 && num < 1000)
+	{
+		a = num / 100;
+		b = (num / 10) % 10;
+		c = num % 10;
+		putchar(a + '0');
+		putchar(b + '0');
+		putchar(c + '0');
+	}
+	else if (num >= 10 && num < 100)
+	{
+		a = num / 10;
+		b = num % 10;
+		putchar(a + '0');
+		putchar(b + '0');
+	}
+	else
+	{
+		putchar(num + '0');
+	}
+}
